f56d2ba95ad44c89a81383b26106a8671f6af722
shaneemichael <shanemichaeltanata@gmail.com>
2025-03-27 22:05:06 +0700

    Implement notify function in Notification service to notify each Subscriber.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -6,7 +6,7 @@ use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
- 
+
 pub struct NotificationService;
 
 impl NotificationService {
@@ -16,6 +16,7 @@ impl NotificationService {
         let subscriber_result: Subscriber = SubscriberRepository::add(product_type_str, subscriber);
         return Ok(subscriber_result);
     }
+    
     pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
         let product_type_upper: String = product_type.to_uppercase();
         let product_type_str: &str = product_type_upper.as_str();
@@ -27,4 +28,22 @@ impl NotificationService {
         }
         return Ok(result.unwrap());
     }
+
+    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
+        let mut payload: Notification = Notification {
+            product_title: product.clone().title,
+            product_type: String::from(product_type),
+            product_url: product.clone().get_url(),
+            subscriber_name: String::from(""),
+            status: String::from(status),
+        };
+    
+        let subscribers: Vec<Subscriber> = SubscriberRepository::list_all(product_type);
+        for subscriber in subscribers {
+            payload.subscriber_name = subscriber.clone().name;
+            let subscriber_clone = subscriber.clone();
+            let payload_clone = payload.clone();
+            thread::spawn(move || subscriber_clone.update(payload_clone));
+        }
+    }    
 }
